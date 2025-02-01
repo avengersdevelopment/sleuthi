ca33b05d11c00e75ff4f59af7bc806f9f794794c
Dharma Yudistira <dharmayudistira2000@gmail.com>
2025-02-01 16:10:03 +0700

    buy button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { choco, hawk, river, useCharacterStore } from "@/store/character-store";
+import { useConfig } from "@/store/config";
 import { cn } from "@/utils/classname";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -26,15 +29,25 @@ export const navigations: NavigationItem[] = [
 
 export const Header = () => {
   const pathname = usePathname();
+  const { character } = useCharacterStore();
+  const buyUrl = useConfig()((state) => state.config?.buy_url);
+
+  const buyButton = {
+    [hawk.name]: "/assets/homepage/header/btn-buy-hawk.png",
+    [choco.name]: "/assets/homepage/header/btn-buy-choco.png",
+    [river.name]: "/assets/homepage/header/btn-buy-river.png",
+  };
 
   return (
     <header className="sticky top-0 z-50 flex w-full justify-center rounded-xl border border-black backdrop-blur-sm">
       <div className="flex w-[90vw] items-center justify-between px-[2vw] py-[1vh]">
         <Link href={"/"}>
-          <p className="md:text-[1.5vw] text-2xl font-bold text-black">sleuthi</p>
+          <p className="text-2xl font-bold text-black md:text-[1.5vw]">
+            sleuthi
+          </p>
         </Link>
 
-        <div className="flex gap-[3vw]">
+        <div className="flex items-center gap-[3vw]">
           {navigations.map((navigation, index) => {
             const isActive = pathname === navigation.link;
 
@@ -42,7 +55,7 @@ export const Header = () => {
               <Link key={index} href={navigation.link}>
                 <p
                   className={cn(
-                    "text-black text-xs md:text-[1vw]",
+                    "text-xs text-black md:text-[1vw]",
                     isActive ? "font-bold" : "font-normal",
                   )}
                 >
@@ -51,6 +64,16 @@ export const Header = () => {
               </Link>
             );
           })}
+
+          <Link href={buyUrl || ""}>
+            <Image
+              src={buyButton[character.name]}
+              width={480}
+              height={480}
+              alt=""
+              className="h-auto w-[20vw] md:w-[10vw] hover:animate-shake"
+            />
+          </Link>
         </div>
       </div>
     </header>
